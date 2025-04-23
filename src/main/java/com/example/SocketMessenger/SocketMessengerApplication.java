e5d71b54f86864bf229109d28a39a1d951b8ac4a
minyoung <myouth_u@naver.com>
2025-04-23 23:24:45 +0900

    add: new project

---
+++ src/main/java/com/example/SocketMessenger/SocketMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocketMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketMessengerApplication.class, args);
+	}
+
+}
