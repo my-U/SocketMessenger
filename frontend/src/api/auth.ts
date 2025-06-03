443e8c52912dc1fdccfbbdba2f7ba08895cd9828
minyoung <myouth_u@naver.com>
2025-06-03 22:58:07 +0900

    feat: 로그인, 회원가입, 아이디 중복 확인 API 연동 메서드 추가

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080";
+
+export interface LoginResponse {
+    token: string;
+}
+
+interface DuplicateCheckResponse {
+    isDuplicate: boolean;
+}
+
+export const login = async (
+    accountId: string,
+    password: string
+): Promise<LoginResponse> => {
+    const res = await axios.post<LoginResponse>(
+        `${BASE_URL}/auth/login`,
+        { accountId, password }
+    );
+    return res.data;
+};
+
+export const register = async (accountId: string, password: string) => {
+    const res = await axios.post(`${BASE_URL}/member/register`, {
+        accountId,
+        password,
+    });
+    return res.data;
+};
+
+export const checkDuplicateId = async (
+    accountId: string
+): Promise<DuplicateCheckResponse> => {
+    const res = await axios.post<DuplicateCheckResponse>(
+        `${BASE_URL}/auth/checkDuplicateId`,
+        { accountId }
+    );
+    return res.data;
+};
